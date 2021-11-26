5086abd1715e7f2bbffd8731327baefc78a39c63
Antonio Castaldo <ancastal@libero.it>
2021-11-26 17:00:55 +0100

    New Interface

--- ProjectWomen2/ContentView.swift
+++ ProjectWomen2/ContentView.swift
@@ -10,45 +10,49 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-//        NavigationView {
+        //        NavigationView {
+        
+        //            ZStack{
+        
+        GeometryReader { metrics in
             
-//            ZStack{
-                
-                VStack {
-                    
-                    
-                    
-                    
-                    Rectangle().background(Color.green)
-                    Rectangle().background(Color.orange)
-                    
-                }
-            }
-//    }
+            VStack(spacing: 0){
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(height:
+                            metrics.size.height * 0.30)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(height: metrics.size.height * 0.90)
+            }.edgesIgnoringSafeArea(.all)
             
-        }
-//    }
-    
-    
-    
-    
-    //                    TabView() {
-    //                        Text("").tabItem {
-    //                            Image(systemName: "house.fill")
-    //                            Text("Home")
-    //                        }.tag(1)
-    //                        Text("").tabItem {
-    //                            Image(systemName: "map.fill")
-    //                            Text("Map")
-    //                        }.tag(2)
-    //                    }
-    
-    
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
         }
     }
+    //    }
+    
+}
+//    }
+
+
+
+
+//                    TabView() {
+//                        Text("").tabItem {
+//                            Image(systemName: "house.fill")
+//                            Text("Home")
+//                        }.tag(1)
+//                        Text("").tabItem {
+//                            Image(systemName: "map.fill")
+//                            Text("Map")
+//                        }.tag(2)
+//                    }
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
