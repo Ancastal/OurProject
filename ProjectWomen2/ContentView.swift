acf1a7fe3727821d7440f5786a5c978606095f78
Antonio Castaldo <ancastal@libero.it>
2021-11-26 16:53:21 +0100

    Changed shapes New

--- ProjectWomen2/ContentView.swift
+++ ProjectWomen2/ContentView.swift
@@ -10,49 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        NavigationView {
+//        NavigationView {
             
-            VStack {
+//            ZStack{
                 
-                ZStack{
+                VStack {
                     
                     
-                    Rectangle()
-                        .background(.blue)
                     
-                    Rectangle()
-                        .background(.white)
                     
-
+                    Rectangle().background(Color.green)
+                    Rectangle().background(Color.orange)
                     
                 }
-                
-                
-                
-                
-                
-                
-                TabView() {
-                    Text("").tabItem {
-                        Image(systemName: "house.fill")
-                        Text("Home")
-                    }.tag(1)
-                    Text("").tabItem {
-                        Image(systemName: "map.fill")
-                        Text("Map")
-                    }.tag(2)
-                }
-                
-                
-                
             }
-            .padding(.bottom)
+//    }
+            
+        }
+//    }
+    
+    
+    
+    
+    //                    TabView() {
+    //                        Text("").tabItem {
+    //                            Image(systemName: "house.fill")
+    //                            Text("Home")
+    //                        }.tag(1)
+    //                        Text("").tabItem {
+    //                            Image(systemName: "map.fill")
+    //                            Text("Map")
+    //                        }.tag(2)
+    //                    }
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
