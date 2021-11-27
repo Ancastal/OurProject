c3f4dc461d6042a846ad3eaadff8724c59e5757f
Antonio Castaldo <ancastal@libero.it>
2021-11-27 02:16:01 +0100

    Quiz UI

---
+++ ProjectWomen2/Color.swift
@@ -0,0 +1,11 @@
+//
+//  Color.swift
+//  ProjectWomen2
+//
+//  Created by Antonio Castaldo on 27/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+let myColor = Color("myCustomBlue")
--- ProjectWomen2/ContentView.swift
+++ ProjectWomen2/ContentView.swift
@@ -15,34 +15,74 @@ struct ContentView: View {
         //            ZStack{
         
         GeometryReader { metrics in
-            Color.orange.ignoresSafeArea()
+            Color.pink.ignoresSafeArea()
             ZStack{
                 
                 VStack(spacing: 0){
                     
                     Rectangle()
-                        .foregroundColor(.blue)
+                        .foregroundColor(myColor)
                         .frame(height:
                                 metrics.size.height * 0.30).overlay(
                                     RoundedRectangle(cornerRadius: 20)
                                         .foregroundColor(.white)
-                                        .frame(width: metrics.size.width * 0.80,
-                                               height: metrics.size.height * 0.20)
+                                        .frame(width: metrics.size.width * 0.8,
+                                               height: metrics.size.height * 0.3)
                                         .offset(x:0, y:120)
-                                        
-
-
+                                    
+                                    
                                 )
-                    
-                    
-                    
-                    
-                }.edgesIgnoringSafeArea(.all)
-            }
+                    Text("What is your experience with \nthis situation?")
+                        .fontWeight(.ultraLight)
+                        .multilineTextAlignment(TextAlignment.center)
+                        .font(.system(size: 20))
+                        .padding(10.0)
+                        .offset(x:0,y:-20)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.white)
+                            .frame(width: metrics.size.width * 0.9,
+                                   height: metrics.size.height * 0.1)
+                            .offset(x:0, y: metrics.size.height * 0.20)
+                        Text("Risposta 1")
+                            .offset(x:0, y: metrics.size.height * 0.2)
+                    }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.white)
+                            .frame(width: metrics.size.width * 0.9,
+                                   height: metrics.size.height * 0.1)
+                            .offset(x:0, y: metrics.size.height * 0.22)
+                        Text("Risposta 2")
+                            .offset(x:0, y: metrics.size.height * 0.22)
+                    }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.white)
+                            .frame(width: metrics.size.width * 0.9,
+                                   height: metrics.size.height * 0.1)
+                            .offset(x:0, y: metrics.size.height * 0.24)
+                        Text("Risposta 3")
+                            .offset(x:0, y: metrics.size.height * 0.24)
+                    }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.white)
+                            .frame(width: metrics.size.width * 0.9,
+                                   height: metrics.size.height * 0.1)
+                            .offset(x:0, y: metrics.size.height * 0.26)
+                        Text("Risposta 4")
+                            .offset(x:0, y: metrics.size.height * 0.26)
+                    }
+                
+                
+                
+            }.edgesIgnoringSafeArea(.all)
         }
     }
-    //    }
-    
+}
+//    }
+
 }
 //    }
 
